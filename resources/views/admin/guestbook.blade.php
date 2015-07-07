98aa0cb07d2ecc8d1f3c45ed087b0c50ca661eb4
Dr. Tobias Quathamer <t.quathamer@mailbox.org>
2015-07-07 10:47:17 +0200

    Use nicer styling for admin page

--- resources/views/admin/guestbook.blade.php
+++ resources/views/admin/guestbook.blade.php
@@ -6,22 +6,21 @@
         Hier können Beiträge angezeigt werden, die eine entsprechende Zuordnung haben.
     </p>
 
-    <div class="container">
-        <a href="{{ action('SpamController@showPosts', ['manual_ham']) }}" class="btn btn-primary">Manuell als Ham gelernt</a>
-        <a href="{{ action('SpamController@showPosts', ['unsure']) }}" class="btn btn-primary">Keine Zuordnung</a>
-        <a href="{{ action('SpamController@showPosts', ['manual_spam']) }}" class="btn btn-primary">Manuell als Spam gelernt</a>
-        <a href="{{ action('SpamController@showPosts', ['autolearn_ham']) }}" class="btn btn-primary">Automatisch als Ham gelernt</a>
-        <a href="{{ action('SpamController@showPosts', ['autolearn_spam']) }}" class="btn btn-primary">Automatisch als Spam gelernt</a>
+    <div class="row">
+        <div class="col-sm-4">
+            <a href="{{ action('SpamController@showPosts', ['manual_ham']) }}" class="btn btn-primary btn-block">Manuell als Ham gelernt</a>
+            <a href="{{ action('SpamController@showPosts', ['unsure']) }}" class="btn btn-primary btn-block">Keine Zuordnung</a>
+            <a href="{{ action('SpamController@showPosts', ['manual_spam']) }}" class="btn btn-primary btn-block">Manuell als Spam gelernt</a>
+            <a href="{{ action('SpamController@showPosts', ['autolearn_ham']) }}" class="btn btn-primary btn-block">Automatisch als Ham gelernt</a>
+            <a href="{{ action('SpamController@showPosts', ['autolearn_spam']) }}" class="btn btn-primary btn-block">Automatisch als Spam gelernt</a>
+        </div>
     </div>
-<!--
-    <p><a href="{{ action('SpamController@showPosts', ['manual_ham']) }}" class="btn btn-primary">Manuell als Ham gelernt</a></p>
-    <p><a href="{{ action('SpamController@showPosts', ['unsure']) }}" class="btn btn-primary">Keine Zuordnung</a></p>
-    <p><a href="{{ action('SpamController@showPosts', ['manual_spam']) }}" class="btn btn-primary">Manuell als Spam gelernt</a></p>
-    <p><a href="{{ action('SpamController@showPosts', ['autolearn_ham']) }}" class="btn btn-primary">Automatisch als Ham gelernt</a></p>
-    <p><a href="{{ action('SpamController@showPosts', ['autolearn_spam']) }}" class="btn btn-primary">Automatisch als Spam gelernt</a></p>
-    -->
     <p>
         Mit diesem Knopf werden alle Texte neu analysiert. Das ist etwas zeitaufwändig.
     </p>
-    <p><a href="{{ action('SpamController@relearn') }}" class="btn btn-primary">Alle Einträge neu lernen</a></p>
+    <div class="row">
+        <div class="col-sm-4">
+            <a href="{{ action('SpamController@relearn') }}" class="btn btn-primary btn-block">Alle Einträge neu lernen</a>
+        </div>
+    </div>
 @stop
