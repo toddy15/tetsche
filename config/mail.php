95d33966bd77489d947abc3b49124f1c14b09301
Dr. Tobias Quathamer <t.quathamer@mailbox.org>
2015-07-09 23:13:36 +0200

    Use application email address

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'webmaster@tetsche.de', 'name' => 'Webmaster'],
 
     /*
     |--------------------------------------------------------------------------
