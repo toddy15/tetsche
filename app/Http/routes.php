2a3d41cfeea27f2c8666c32325a2e377a33780ce
Dr. Tobias Quathamer <t.quathamer@mailbox.org>
2015-07-09 23:04:20 +0200

    Disable registration routes and do not show login link in navbar

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -6,8 +6,8 @@ Route::post('auth/login', 'Auth\AuthController@postLogin');
 Route::get('auth/logout', 'Auth\AuthController@getLogout');
 
 // Registration routes...
-Route::get('auth/register', 'Auth\AuthController@getRegister');
-Route::post('auth/register', 'Auth\AuthController@postRegister');
+//Route::get('auth/register', ['middleware' => 'auth', 'uses' => 'Auth\AuthController@getRegister']);
+//Route::post('auth/register', ['middleware' => 'auth', 'uses' => 'Auth\AuthController@postRegister']);
 
 // Password reset link request routes...
 Route::get('password/email', 'Auth\PasswordController@getEmail');
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -17,20 +17,17 @@
                 <li {!! Request::is('impressum') ? 'class="active"' : '' !!}><a href="{{ url('impressum') }}">Impressum</a></li>
             </ul>
 
-            <ul class="nav navbar-nav navbar-right">
-                @if (Auth::guest())
-                    <li><a href="{{ url('/auth/login') }}">Login</a></li>
-                    <li><a href="{{ url('/auth/register') }}">Register</a></li>
-                @else
+            @unless (Auth::guest())
+                <ul class="nav navbar-nav navbar-right">
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
                         <ul class="dropdown-menu" role="menu">
-                            <li><a href="{{ action('SpamController@index') }}">Administration des Gästebuch</a></li>
+                            <li><a href="{{ action('SpamController@index') }}">Administration des Gästebuchs</a></li>
                             <li><a href="{{ url('/auth/logout') }}">Logout</a></li>
                         </ul>
                     </li>
-                @endif
-            </ul>
+                </ul>
+            @endunless
         </div>
     </div>
 </nav>
