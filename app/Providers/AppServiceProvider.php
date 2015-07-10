4c563ed217256274bfdea9f5fcab1ebcfb4256da
Dr. Tobias Quathamer <t.quathamer@mailbox.org>
2015-07-10 11:58:38 +0200

    Use setlocale for the Carbon datetime library

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Set locale, needed for Carbon DateTime library
+        setlocale(LC_TIME, 'de_DE.UTF-8');
     }
 
     /**
