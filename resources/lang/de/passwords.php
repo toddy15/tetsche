e819a9ce6c43683e612cc5cc1724be195aca0153
Dr. Tobias Quathamer <t.quathamer@mailbox.org>
2015-07-17 19:35:31 +0200

    Add German translation for some strings

---
+++ resources/lang/de/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reminder Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Passwörter müssen mindestens sechs Zeichen haben und mit der Bestätigung übereinstimmen.',
+    'user' => "Es gibt keinen Benutzer mit dieser E-Mail-Adresse.",
+    'token' => 'Dieses Token für die Zurücksetzung eines Passworts ist ungültig.',
+    'sent' => 'Wir haben eine E-Mail mit dem Link für das Zurücksetzen des Passworts verschickt.',
+    'reset' => 'Ihr Passwort wurde zurückgesetzt!',
+
+];
