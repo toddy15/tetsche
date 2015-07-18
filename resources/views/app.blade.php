531cfa65f6c71f3f18a15a54031b801413207226
Dr. Tobias Quathamer <t.quathamer@mailbox.org>
2015-07-18 20:29:54 +0200

    Add piwik tracking code

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -39,5 +39,20 @@
 
         <script src="{{ asset(elixir('theme/js/all.js')) }}"></script>
         @include('additional_javascript')
+        <!-- Piwik -->
+        <script type="text/javascript">
+            var _paq = _paq || [];
+            _paq.push(['trackPageView']);
+            _paq.push(['enableLinkTracking']);
+            (function() {
+                var u="//www.tetsche.de/piwik/";
+                _paq.push(['setTrackerUrl', u+'piwik.php']);
+                _paq.push(['setSiteId', 1]);
+                var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+            })();
+        </script>
+        <noscript><p><img src="//www.tetsche.de/piwik/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
+        <!-- End Piwik Code -->
     </body>
 </html>
