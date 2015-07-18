074b73b8c38a314fa301abba438461575deafc8d
Dr. Tobias Quathamer <t.quathamer@mailbox.org>
2015-07-18 17:06:05 +0200

    Add more space below paragraph for hund.jpg

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
     <h1 style="font-size: 81px;">Tetsche-Website</h1>
-    <p style="margin-bottom:15px;font-size:27px;font-weight:200">Neu! Jetzt auch in weiß!</p>
+    <p style="margin-bottom:130px;font-size:27px;font-weight:200">Neu! Jetzt auch in weiß!</p>
     <img class="center-block img-responsive" src="{{ asset('images/hund.jpg') }}" alt="Hund" title="Hund" width="539" height="359" />
 @stop
