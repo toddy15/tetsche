3f3317dcf9f7d1f6d4ba810212b35ae16e6f0ff8
Dr. Tobias Quathamer <t.quathamer@mailbox.org>
2015-07-18 16:42:00 +0200

    Do not use jumbotron on homepage and add hund.jpg

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -1,8 +1,7 @@
 @extends('app')
 
 @section('content')
-    <div class="jumbotron">
-        <h1>Tetsche-Website</h1>
-        <p>Neu! Jetzt auch in weiß!</p>
-    </div>
+    <h1 style="font-size: 81px;">Tetsche-Website</h1>
+    <p style="margin-bottom:15px;font-size:27px;font-weight:200">Neu! Jetzt auch in weiß!</p>
+    <img class="center-block img-responsive" src="{{ asset('images/hund.jpg') }}" alt="Hund" title="Hund" width="539" height="359" />
 @stop
