d331120eaafd82a4e36730f83d112b97d0ed4457
Dr. Tobias Quathamer <toddy@debian.org>
2017-03-17 18:12:22 +0100

    Add new ausstellung havengalerie

--- resources/views/pages/ausstellung.blade.php
+++ resources/views/pages/ausstellung.blade.php
@@ -3,26 +3,29 @@
 @section('content')
     <h1 style="color:#fdb3b4;font-size:81px;margin-bottom:80px;">Tetsche-Website</h1>
     <img class="center-block img-responsive" src="{{ asset('images/hund-grinsend.jpg') }}" alt="Hund" title="Hund" width="729" height="1204" />
-    <h2 class="text-center" style="color:#fdb3b4">Ausstellung im Caricatura Museum Frankfurt</h2>
+    <h2 class="text-center" style="color:#fdb3b4">Ausstellung in der havengalerie Bremen</h2>
     <div class="container" style="margin-bottom:20px;">
         <div class="row">
             <div class="col-md-6">
-                <img class="center-block img-responsive" src="{{ asset('images/stern-bilder.jpg') }}" alt="Ausstellung" title="Ausstellung" width="500" height="690" />
+                <img class="center-block img-responsive" src="{{ asset('images/havengalerie.jpg') }}" alt="Ausstellung" title="Ausstellung" width="538" height="1182" />
             </div>
             <div class="col-md-6">
                 <p>
-                    Die Ausstellung zeigt neben den Klassikern vor allem
-                    die aktuell im stern vertretenen Zeichner und Humoristen.
-                    Im Mittelpunkt werden die drei großen Komischen Künstler
-                    Gerhard Haderer, Til Mette und Tetsche stehen.
+                    25. März bis 27. Mai<br />
+                    Vernissage am 24. März um 18:00 Uhr
+                </p>
+                <p>
+                    Öffnungszeiten:<br />
+                    Dienstag &ndash; Freitag von 10:00 &ndash; 12:30 Uhr und 15:00 &ndash; 18:00 Uhr<br />
+                    Samstag von 10:00 &ndash; 14:00 Uhr<br />
+                    Eintritt frei
                 </p>
                 <address>
-                    <strong>Caricatura Museum Frankfurt</strong><br />
-                    Museum für Komische Kunst<br />
-                    Weckmarkt 17<br />
-                    60311 Frankfurt am Main<br />
-                    Tel.: <a style="color:#ff0" href="tel:+49-69-21230161">+49 69 212 301 61</a><br />
-                    <a style="color:#ff0" href="http://caricatura-museum.de/">www.caricatura-museum.de</a>
+                    <strong>havengalerie</strong><br />
+                    Alte Hafenstraße 27<br />
+                    28757 Bremen<br />
+                    Tel.: <a style="color:#ff0" href="tel:+49-421-69200896">+49 421 69200896</a><br />
+                    <a style="color:#ff0" href="https://www.havengalerie.de/">www.havengalerie.de</a>
                 </address>
             </div>
         </div>
