8674de4005e5536943df6274109781d9834adc63
Dr. Tobias Quathamer <toddy@debian.org>
2017-12-23 17:33:51 +0100

    Add Eisbärstory for christmas

--- resources/views/pages/sonderseite.blade.php
+++ resources/views/pages/sonderseite.blade.php
@@ -2,5 +2,34 @@
 
 @section('content')
     <h1 style="color:#fdb3b4;font-size:81px;margin-bottom:80px;">Tetsche-Website</h1>
-    <img class="center-block img-responsive" src="{{ asset('images/kerze.jpg') }}" alt="Kerze" title="Kerze" width="465" height="521" />
+
+    <div class="row">
+        <div class="col-sm-6">
+            <img class="center-block img-responsive" src="{{ asset('images/eisbaer.jpg') }}" alt="Eisbär" title="Eisbär" width="401" height="585" />
+        </div>
+        <div class="col-sm-6">
+            Es geschah auf dem weltberühmten Soltauer Weihnachtsmarkt!
+            Zu der Zeit trieben runtergekommene, übelriechende, zottelige
+            Eisbär-Gesellen auf ziemlich allen Weihnachtsmärkten ihr
+            Unwesen. Indem sie blitzartig&nbsp;– aus dem Nichts heraus&nbsp;– kleine,
+            unschuldige Knaben mit einem Prankenhieb an sich rissen,
+            die Eltern blöde angrinsten und lautstark grölend 50 Pfennig
+            Lösegeld forderten. Und hier (siehe Foto) war ich sein Opfer!
+            »Wenn Ihr nicht zahlt, zerquetsch ich diesen kleinen Tetsche!«,
+            knurrte er meine sichtlich belustigten Eltern an und roch dabei
+            nach extrem stark verzimten Glühwein. Mir wurde schlecht! Das
+            bekam Oma Anna mit, stürzte sich mit Gebrüll auf den Bär, zog
+            ihren Schlagschirm aus dem Gürtel und drosch mit den Worten:
+            »So nicht, Bürschchen!« auf den hart stinkenden Zottel ein.
+            Ich war schlagartig befreit! Aber der Lodenmantel war dahin,
+            der Tirolerhut auch, und meine Manchester-Büx hatte sich durch
+            den üblen Bärengeruch stark gewellt und auch teilweise aufgerollt.
+            »Halb so schlimm!«, tröstete mich meine Oma. »Kuck mal, da
+            vorne brennt noch Licht bei Gülcher&nbsp;&amp;&nbsp;Co. Da gehn wir jetzt hin,
+            und wenn Du möchtest, spendier ich Dir eine tolle Nietenhose!«
+            »Oma, Oma, Du bist die Beste!« tirilierte ich in höchsten Tönen.
+            »Das ist das schönste und beste Weihnachtsgeschenk aller Zeiten!«
+        </div>
+    </div>
+
 @stop
