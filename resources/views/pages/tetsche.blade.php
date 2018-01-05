a4ad43ab80265133648dd2f1860766f2a00de72b
Dr. Tobias Quathamer <toddy@debian.org>
2018-01-05 21:37:24 +0100

    New text for about page

--- resources/views/pages/tetsche.blade.php
+++ resources/views/pages/tetsche.blade.php
@@ -1,49 +1,55 @@
 @extends('app')
 
 @section('content')
-<div class="row">
+<div class="row" style="margin-bottom: 0.7em;">
     <div class="col-md-12">
         <img class="center-block" src="{{ asset('images/tetsche.jpg') }}" />
     </div>
 </div>
 <div class="row">
     <div class="col-xs-12 col-sm-6">
-        <span class="hidden-xs">
-          <div class="text-right">
-              Tetsche wurde in Soltau, mitten in der Lüneburger Heide, geboren.
-              Während seiner Lehr- und Wanderjahre arbeitete er als
-              Pflastermaler in Amsterdam, als Layouter bei einer großen Tageszeitung in Köln
-              und schließlich als Kreativ-Direktor in einer Hamburger Werbeagentur.
-              Bis ihn die unbändige Lust überkam, sich als Cartoonist
-              selbstständig zu machen. Erste Zeichnungen
-              veröffentlichte er in der »Zeit«, »Konkret«,
-              »Pardon« und »Hörzu«.
-          </div>
-        </span>
-        <span class="hidden-sm hidden-md hidden-lg">
-          Tetsche wurde in Soltau, mitten in der Lüneburger Heide, geboren.
-          Während seiner Lehr- und Wanderjahre arbeitete er als
-          Pflastermaler in Amsterdam, als Layouter bei einer großen Tageszeitung in Köln
-          und schließlich als Kreativ-Direktor in einer Hamburger Werbeagentur.
-          Bis ihn die unbändige Lust überkam, sich als Cartoonist
-          selbstständig zu machen. Erste Zeichnungen
-          veröffentlichte er in der »Zeit«, »Konkret«,
-          »Pardon« und »Hörzu«.
-        </span>
+        <div class="hidden-xs text-right">
+            Tetsche wurde in Soltau, inmitten der Lüneburger Heide, geboren.
+            Während seiner Lehr- und Wanderjahre arbeitete er als
+            Pflastermaler in Amsterdam, als Layouter bei einer großen Tageszeitung in Köln
+            und schließlich als Creative Director in einer Hamburger Werbeagentur.
+            Bis ihn die unbändige Lust überkam, sich als Cartoonist
+            selbstständig zu machen. Erste Zeichnungen
+            veröffentlichte er in der »Zeit«, »Konkret«,
+            »Pardon« und »Hörzu«.
+            Für den »Stern« erfand er die Kult-Seite
+            »Neues aus Kalau«, die seitdem dort jede Woche für
+            die unzähligen Fans von Pümpel,
+        </div>
+        <div class="hidden-sm hidden-md hidden-lg">
+            Tetsche wurde in Soltau, inmitten der Lüneburger Heide, geboren.
+            Während seiner Lehr- und Wanderjahre arbeitete er als
+            Pflastermaler in Amsterdam, als Layouter bei einer großen Tageszeitung in Köln
+            und schließlich als Creative Director in einer Hamburger Werbeagentur.
+            Bis ihn die unbändige Lust überkam, sich als Cartoonist
+            selbstständig zu machen. Erste Zeichnungen
+            veröffentlichte er in der »Zeit«, »Konkret«,
+            »Pardon« und »Hörzu«.
+            Für den »Stern« erfand er die Kult-Seite
+            »Neues aus Kalau«, die seitdem dort jede Woche für
+            die unzähligen Fans von Pümpel,
+        </div>
     </div>
     <div class="col-sm-6">
-        Für den »Stern« erfand er die Kult-Seite
-        »Neues aus Kalau«, die seitdem dort jede Woche für
-        die unzähligen Fans von Pümpel, Spiegelei und
-        Konsorten exklusiv erscheint!
+        Spiegelei und
+        Konsorten exklusiv erscheint.
         Tetsche erhielt zahlreiche Preise und Auszeichnungen,
         u.&nbsp;a. den Deutschen Zynikerpreis in Gold,
         Silbermedaille des Art Directors Club,
         den Goldenen Jupp am Bande,
         die Marion-Möser-Medaille,
-        den Goldenen Wink und
-        den Publikumspreis des Deutschen Karikaturenpreises!
-        Tetsche lebt an der Elbe, in der Nähe von Hamburg.
+        den Apfelbaum vom Lande,
+        den goldenen Wink, mehrmals
+        den Publikumspreis des Deutschen Karikaturenpreises
+        und den 1. Platz des Deutschen Cartoonpreises 2017
+        der Frankfurter Buchmesse.
+        Tetsche lebt hinterm Deich an der Elbe, in der Nähe von Hamburg,
+        der schönsten Stadt der Welt!
     </div>
 </div>
 @stop
