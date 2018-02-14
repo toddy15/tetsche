e855eef7b4c89d7b08648e9cc6d83d05934976bb
Dr. Tobias Quathamer <toddy@debian.org>
2018-02-14 21:00:05 +0100

    Add another fuckhead

--- app/Http/Controllers/GuestbookPostsController.php
+++ app/Http/Controllers/GuestbookPostsController.php
@@ -78,6 +78,9 @@ class GuestbookPostsController extends Controller
             if (($ip[0] == 93) and ($ip[1] == 214) and ($ip[2] == 118)) {
                 $post['score'] = $spamfilter->threshold_autolearn_spam;
             }
+            if (($ip[0] == 79) and ($ip[1] == 232) and ($ip[2] == 145)) {
+                $post['score'] = $spamfilter->threshold_autolearn_spam;
+            }
         }
         $post['category'] = $spamfilter->calculateCategory($post['score']);
         $post['spam_detection'] = $spam_detection;
