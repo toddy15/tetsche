7931a07cb465dddfa96ea0dad678448e34a6c1a6
Dr. Tobias Quathamer <toddy@debian.org>
2018-04-04 22:04:56 +0200

    Remove datenschutz from impressum

--- resources/views/pages/impressum.blade.php
+++ resources/views/pages/impressum.blade.php
@@ -21,51 +21,14 @@
     Baumwall 11<br />
     20459 Hamburg<br />
 </p>
-<h3 style="color:#fdb3b4;">Hinweise zum Datenschutz</h3>
+<h3 style="color:#fdb3b4;">Widerspruch Werbe-Mails</h3>
 <p>
-    Die Sicherheit Ihrer persönlichen Daten ist uns wichtig.
-    Aus diesem Grund werden die von Ihrem Browser automatisch
-    übermittelten Daten (u.a. Ihre IP-Adresse) <strong>nicht</strong>
-    von uns in Protokolldateien gespeichert.
-</p>
-<h3 style="color:#fdb3b4;">Piwik / Google Analytics</h3>
-<p>
-    Wir setzen Piwik für eine statistische Auswertung
-    der aufgerufenen Seiten ein. Sämtliche erfassten Daten bleiben
-    dabei auf unserem Server und werden nicht an Dritte
-    weitergegeben. Ihre IP-Adresse wird hierbei anonymisiert.
-    Die Zuordnung der Daten zu einer bestimmten Person ist uns nicht
-    möglich.
-</p>
-<p>
-    Wenn Sie in Ihrem Browser »Ich möchte nicht getrackt werden«
-    eingestellt haben (Do-not-Track aktiviert), dann werden Ihre Besuche
-    dieser Website von Piwik nicht aufgezeichnet.
-</p>
-<p>
-    Auf den Einsatz von Google Analytics verzichten wir.
-</p>
-<h3 style="color:#fdb3b4;">Cookies</h3>
-<p>
-    Wir verwenden Cookies, um auf dieser Website zusätzliche Funktionen
-    anzubieten. Sie können in Ihrem Browser einstellen,
-    ob Sie Cookies auf Ihrem Rechner zulassen wollen oder nicht.
-    Hauptsächlich verwenden wir »Session-Cookies«, die nach dem
-    Besuch unserer Website automatisch gelöscht werden.
-</p>
-<h3 style="color:#fdb3b4;">Personenbezogene Daten</h3>
-<p>
-    Die Nutzung dieser Website ist ohne Angabe personenbezogener
-    Daten möglich. Soweit auf unseren Seiten personenbezogene
-    Daten erhoben werden, erfolgt dies grundsätzlich auf
-    freiwilliger Basis.
-</p>
-<h3 style="color:#fdb3b4;">Auskunftsrecht</h3>
-<p>
-    Sie haben das Recht, von uns unentgeltlich Auskunft über
-    die zu Ihrer Person gespeicherten Daten sowie deren Herkunft
-    und Empfänger zu erhalten. Weiterhin haben Sie das Recht
-    auf Berichtigung, Sperrung oder Löschung dieser Daten.
-    Ihre Anfrage richten Sie bitte an unsere oben genannten Adressen.
+    Der Nutzung von im Rahmen der Impressumspflicht
+    veröffentlichten Kontaktdaten zur Übersendung von
+    nicht ausdrücklich angeforderter Werbung und
+    Informationsmaterialien wird hiermit widersprochen.
+    Die Betreiber der Seiten behalten sich ausdrücklich
+    rechtliche Schritte im Falle der unverlangten Zusendung
+    von Werbeinformationen, etwa durch Spam-E-Mails, vor.
 </p>
 @stop
