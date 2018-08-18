a8bb58f2e690f3e6737ef9f9b916da39c8bace4c
Dr. Tobias Quathamer <toddy@debian.org>
2018-08-18 22:33:23 +0200

    Show ausstellung only until end date

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -11,13 +11,13 @@ class PagesController extends Controller
 {
     public function homepage()
     {
-        if (date("Y-m-d") <= "2018-07-19") {
-            return view('pages.sonderseite', [
+        if (date("Y-m-d") <= "2018-09-09") {
+            return view('pages.sonderseite-juli', [
                 'description' => 'Tetsche-Website',
             ]);
         }
         if (date("Y-m-d") <= "2018-09-16") {
-            return view('pages.sonderseite-juli', [
+            return view('pages.sonderseite', [
                 'description' => 'Tetsche-Website',
             ]);
         }
