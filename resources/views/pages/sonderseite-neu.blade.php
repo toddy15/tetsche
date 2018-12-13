d37068cdd7dd65e3c2fd5e0fc6d22242cd9cf2bc
Dr. Tobias Quathamer <toddy@debian.org>
2018-12-13 22:32:31 +0100

    Replace image on homepage

--- resources/views/pages/sonderseite-neu.blade.php
+++ resources/views/pages/sonderseite-neu.blade.php
@@ -3,6 +3,6 @@
 @section('content')
     <h1 style="color:#fdb3b4;font-size:81px;margin-bottom:80px;">Tetsche-Website</h1>
     <div style="margin-bottom:80px;">
-        <img class="center-block img-responsive" src="{{ asset('images/rausgeflogen.jpg') }}" alt="Tetsche-Seite ist rausgeflogen" title="Tetsche-Seite ist rausgeflogen" width="780" height="709" />
+        <img class="center-block img-responsive" src="{{ asset('images/erklaerung.jpg') }}" alt="Tetsche-Seite ist rausgeflogen" title="Tetsche-Seite ist rausgeflogen" width="681" height="539" />
     </div>
 @stop
