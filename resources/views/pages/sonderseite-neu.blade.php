d554565bf2e37ecf5e37caeaa7f00576d5d338fd
Dr. Tobias Quathamer <toddy@debian.org>
2018-12-16 20:55:34 +0100

    Use transparent background on startpage

--- resources/views/pages/sonderseite-neu.blade.php
+++ resources/views/pages/sonderseite-neu.blade.php
@@ -3,6 +3,6 @@
 @section('content')
     <h1 style="color:#fdb3b4;font-size:81px;margin-bottom:80px;">Tetsche-Website</h1>
     <div style="margin-bottom:80px;">
-        <img class="center-block img-responsive" src="{{ asset('images/erklaerung.jpg') }}" alt="Tetsche-Seite ist rausgeflogen" title="Tetsche-Seite ist rausgeflogen" width="681" height="539" />
+        <img class="center-block img-responsive" src="{{ asset('images/erklaerung-transparent.png') }}" alt="Tetsche-Seite ist rausgeflogen" title="Tetsche-Seite ist rausgeflogen" width="681" height="539" />
     </div>
 @stop
