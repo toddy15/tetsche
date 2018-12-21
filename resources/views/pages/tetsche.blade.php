6d095114a3a59619cbb126e111f7f337feb1f530
Dr. Tobias Quathamer <toddy@debian.org>
2018-12-21 21:14:09 +0100

    Better wording for about page

--- resources/views/pages/tetsche.blade.php
+++ resources/views/pages/tetsche.blade.php
@@ -10,52 +10,52 @@
     <div class="col-xs-12 col-sm-6">
         <div class="hidden-xs text-right">
             <p>
-                Tetsche veröffentlicht seinen ersten Cartoon im zarten Alter
+                Tetsche veröffentlichte seinen ersten Cartoon im zarten Alter
                 von 16 Jahren in der »Hör&nbsp;zu«. Nach der Schulzeit und einer
-                Lehre als Schriftsetzer arbeitet er dann&nbsp;&ndash; während
+                Lehre als Schriftsetzer arbeitete er dann&nbsp;&ndash; während
                 seiner Lehr- und Wanderjahre&nbsp;&ndash; als Pflastermaler
                 in Amsterdam, als Layouter bei einer großen Tageszeitung
-                in Köln, grast diverse kleine Agenturen ab und landet
+                in Köln, klapperte diverse kleine Agenturen ab und landete
                 schließlich als Grafiker und Creative Director in einer
                 großen Hamburger Werbeagentur.
             </p>
             <p>
-                Bis ihn die unbändige Lust überkommt, sich als Cartoonist
-                selbstständig zu machen. Erste Zeichnungen erscheinen in
+                Bis ihn die unbändige Lust überkam, sich als Cartoonist
+                selbstständig zu machen. Erste Zeichnungen erschienen in
                 »Pardon«, »Zeit«, »konkret« und »Hör&nbsp;zu«.
-                Für den »stern« erfindet er die Kultseite »Neues&nbsp;aus&nbsp;Kalau«,
-                die dort 40 Jahre lang wöchentlich exklusiv erscheint und
+                Für den »stern« erfand er die Kultseite »Neues&nbsp;aus&nbsp;Kalau«,
+                die dort 40 Jahre lang wöchentlich exklusiv erschienen ist und
                 unzählige Fans nicht nur von den Cartoons, sondern auch
                 von Kuhno van Oyten, dem Abgeschlossenen Roman,
                 Bauernweisheiten, dem Pümpel, Spiegelei und Konsorten hat.
             </p>
         </div>
         <div class="hidden-sm hidden-md hidden-lg">
-          <p>
-              Tetsche veröffentlicht seinen ersten Cartoon im zarten Alter
-              von 16 Jahren in der »Hör&nbsp;zu«. Nach der Schulzeit und einer
-              Lehre als Schriftsetzer arbeitet er dann&nbsp;&ndash; während
-              seiner Lehr- und Wanderjahre&nbsp;&ndash; als Pflastermaler
-              in Amsterdam, als Layouter bei einer großen Tageszeitung
-              in Köln, grast diverse kleine Agenturen ab und landet
-              schließlich als Grafiker und Creative Director in einer
-              großen Hamburger Werbeagentur.
-          </p>
-          <p>
-              Bis ihn die unbändige Lust überkommt, sich als Cartoonist
-              selbstständig zu machen. Erste Zeichnungen erscheinen in
-              »Pardon«, »Zeit«, »konkret« und »Hör&nbsp;zu«.
-              Für den »stern« erfindet er die Kultseite »Neues&nbsp;aus&nbsp;Kalau«,
-              die dort 40 Jahre lang wöchentlich exklusiv erscheint und
-              unzählige Fans nicht nur von den Cartoons, sondern auch
-              von Kuhno van Oyten, dem Abgeschlossenen Roman,
-              Bauernweisheiten, dem Pümpel, Spiegelei und Konsorten hat.
-          </p>
+            <p>
+                Tetsche veröffentlichte seinen ersten Cartoon im zarten Alter
+                von 16 Jahren in der »Hör&nbsp;zu«. Nach der Schulzeit und einer
+                Lehre als Schriftsetzer arbeitete er dann&nbsp;&ndash; während
+                seiner Lehr- und Wanderjahre&nbsp;&ndash; als Pflastermaler
+                in Amsterdam, als Layouter bei einer großen Tageszeitung
+                in Köln, klapperte diverse kleine Agenturen ab und landete
+                schließlich als Grafiker und Creative Director in einer
+                großen Hamburger Werbeagentur.
+            </p>
+            <p>
+                Bis ihn die unbändige Lust überkam, sich als Cartoonist
+                selbstständig zu machen. Erste Zeichnungen erschienen in
+                »Pardon«, »Zeit«, »konkret« und »Hör&nbsp;zu«.
+                Für den »stern« erfand er die Kultseite »Neues&nbsp;aus&nbsp;Kalau«,
+                die dort 40 Jahre lang wöchentlich exklusiv erschienen ist und
+                unzählige Fans nicht nur von den Cartoons, sondern auch
+                von Kuhno van Oyten, dem Abgeschlossenen Roman,
+                Bauernweisheiten, dem Pümpel, Spiegelei und Konsorten hat.
+            </p>
         </div>
     </div>
     <div class="col-sm-6">
         <p>
-            Tetsche hat zahlreiche Preise und Auszeichnungen, u.&nbsp;a.
+            Tetsche hat zahlreiche Preise und Auszeichnungen erhalten, u.&nbsp;a.
             zweimal die Silbermedaille des Art Directors Club Deutschland,
             bereits mehrmals den Publikumspreis des Deutschen
             Karikaturenpreises und den 1.&nbsp;Platz des Deutschen
