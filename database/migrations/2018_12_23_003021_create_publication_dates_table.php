fe2964837f8fdbfaa8a3692a7e54b52f00bf0839
Dr. Tobias Quathamer <toddy@debian.org>
2018-12-23 00:30:44 +0100

    Run php artisan make:model -m PublicationDate

---
+++ database/migrations/2018_12_23_003021_create_publication_dates_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePublicationDatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('publication_dates', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('publication_dates');
+    }
+}
