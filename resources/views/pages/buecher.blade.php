b3741ed75793ed21505fbeb611631ea2d4485390
Dr. Tobias Quathamer <toddy@debian.org>
2018-12-24 23:56:24 +0100

    Add more buecher

--- resources/views/pages/buecher.blade.php
+++ resources/views/pages/buecher.blade.php
@@ -4,8 +4,8 @@
 <div class="row">
     <div class="col-xs-12 col-sm-6">
         <a href="https://www.amazon.de/Cartoons-andere-Kostbarkeiten-Tetsche/dp/3830335164/">
-            <img class="center-block img-responsive" src="{{ asset('images/gesundheit.jpg') }}"
-                 alt="Tetsche &ndash; Cartoons und andere Kostbarkeiten" title="Tetsche &ndash; Cartoons und andere Kostbarkeiten" width="596" height="842" />
+            <img class="center-block img-responsive" src="{{ asset('images/buecher/gesundheit.jpg') }}"
+                 alt="Tetsche &ndash; Cartoons und andere Kostbarkeiten" title="Tetsche &ndash; Cartoons und andere Kostbarkeiten" width="480" height="595" />
         </a>
     </div>
     <div class="col-md-6">
@@ -30,8 +30,8 @@
 <div class="row">
     <div class="col-xs-12 col-sm-6">
         <a href="https://www.amazon.de/Platsch-Fridos-Sprung-ins-Abenteuer/dp/3830312849/">
-            <img class="center-block img-responsive" src="{{ asset('images/platsch-cover.jpg') }}"
-                 alt="Platsch! Fridos Sprung ins Abenteuer" title="Platsch! Fridos Sprung ins Abenteuer" width="660" height="807" />
+            <img class="center-block img-responsive" src="{{ asset('images/buecher/platsch.jpg') }}"
+                 alt="Platsch! Fridos Sprung ins Abenteuer" title="Platsch! Fridos Sprung ins Abenteuer" width="480" height="588" />
         </a>
     </div>
     <div class="col-md-6">
@@ -57,6 +57,70 @@
         </p>
         <p>
             <a href="https://www.amazon.de/Platsch-Fridos-Sprung-ins-Abenteuer/dp/3830312849/" class="btn btn-danger">Bei Amazon ansehen</a>
+        </p>
+    </div>
+</div>
+<div class="row">
+    <div class="col-xs-12 col-sm-6">
+        <a href="https://www.amazon.de/Hoffentlich-ist-es-nichts-Ernstes/dp/3830332459">
+            <img class="center-block img-responsive" src="{{ asset('images/buecher/hoffentlich-nichts-ernstes.jpg') }}"
+                 alt="Hoffentlich ist es nichts Ernstes!" title="Hoffentlich ist es nichts Ernstes!" width="480" height="581" />
+        </a>
+    </div>
+    <div class="col-md-6">
+        <h2>Hoffentlich ist es nichts Ernstes!</h2>
+        <p>
+            ISBN 978-3-8303-3245-9<br/>
+            Lappan / Carlsen Verlag<br/>
+            € 19,95
+        </p>
+        <p>
+            <a href="https://www.amazon.de/Hoffentlich-ist-es-nichts-Ernstes/dp/3830332459" class="btn btn-danger">Bei Amazon ansehen</a>
+        </p>
+    </div>
+</div>
+<div class="row">
+    <div class="col-xs-12 col-sm-6">
+        <a href="https://www.amazon.de/Beste-Bilder-8-Deutscher-Cartoonpreis/dp/3830334591">
+            <img class="center-block img-responsive" src="{{ asset('images/buecher/beste-bilder.jpg') }}"
+                 alt="Beste Bilder 8 (Deutscher Cartoonpreis)" title="Beste Bilder 8 (Deutscher Cartoonpreis)" width="480" height="581" />
+        </a>
+    </div>
+    <div class="col-md-6">
+        <h2>Beste Bilder 8 (Deutscher Cartoonpreis)</h2>
+        <p>
+            ISBN 978-3-8303-3459-0<br/>
+            Lappan / Carlsen Verlag<br/>
+            € 12,00
+        </p>
+        <p>
+            <a href="https://www.amazon.de/Beste-Bilder-8-Deutscher-Cartoonpreis/dp/3830334591" class="btn btn-danger">Bei Amazon ansehen</a>
+        </p>
+    </div>
+</div>
+<div class="row">
+    <div class="col-xs-12 col-sm-6">
+        <img class="center-block img-responsive" src="{{ asset('images/buecher/alles-ueber-aerzte.jpg') }}"
+             alt="Alles über Ärzte" title="Alles über Ärzte" width="397" height="482" />
+    </div>
+    <div class="col-md-6">
+        <h2>Alles über Ärzte</h2>
+        <p>
+            ISBN 978-3-8303-4026-3<br/>
+            Lappan / Carlsen Verlag<br/>
+        </p>
+    </div>
+</div>
+<div class="row">
+    <div class="col-xs-12 col-sm-6">
+        <img class="center-block img-responsive" src="{{ asset('images/buecher/roman.jpg') }}"
+             alt="Der abgeschlossene Roman" title="Der abgeschlossene Roman" width="397" height="482" />
+    </div>
+    <div class="col-md-6">
+        <h2>Der abgeschlossene Roman</h2>
+        <p>
+            ISBN 978-3-8303-6048-3<br/>
+            Lappan / Carlsen Verlag<br/>
         </p>
     </div>
 </div>
