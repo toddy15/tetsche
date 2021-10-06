0c2b5256d6e1c410ce30dee2599b287404f532e6
Dr. Tobias Quathamer <toddy@debian.org>
2021-10-06 23:03:59 +0200

    Remove Mühle Mahlstedt

--- resources/views/pages/sonderseite.blade.php
+++ resources/views/pages/sonderseite.blade.php
@@ -3,11 +3,6 @@
 @section('content')
     <h1 style="color:#fdb3b4;font-size:81px;margin-bottom:80px;">Tetsche-Website</h1>
     <div style="text-align:center;margin-bottom:2em;">
-        <a href="https://www.muehle-malstedt-kunstwerkstatt.de/">
-            <img src="images/muehle-malstedt.jpg" width="842" height="1191" alt="Ausstellung Mühle Malstedt Kunstwerkstatt"/>
-        </a>
-    </div>
-    <div style="text-align:center;margin-bottom:2em;">
         <a href="https://www.stade-tourismus.de/de/tetsche-ausstellung-im-hansehafen">
             <img src="images/open-air.jpg" width="842" height="1191" alt="Ausstellung Stade Open Air"/>
         </a>
