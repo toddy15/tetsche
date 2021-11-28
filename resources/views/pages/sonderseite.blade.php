ec95451d2b1ebf8cb9a61f2b6231de3c3b730b70
Dr. Tobias Quathamer <toddy@debian.org>
2021-11-28 14:56:34 +0100

    Add buch about open air

--- resources/views/pages/sonderseite.blade.php
+++ resources/views/pages/sonderseite.blade.php
@@ -2,9 +2,14 @@
 
 @section('content')
     <h1 style="color:#fdb3b4;font-size:81px;margin-bottom:80px;">Tetsche-Website</h1>
+    <p style="font-size: 24px;text-align:center;margin-bottom:2em">
+    Es gibt ein neues Buch!<br>
+    Alles über die sensationelle Open Air Ausstellung im Stader Hansehafen<br>
+    ISBN 978-3-96194-173-5
+    </p>
     <div style="text-align:center;margin-bottom:2em;">
-        <a href="https://www.stade-tourismus.de/de/tetsche-ausstellung-im-hansehafen">
-            <img src="images/open-air.jpg" width="842" height="1191" alt="Ausstellung Stade Open Air"/>
+        <a href="https://www.kjm-buchverlag.de/produkt/tetsche-open-air/">
+            <img src="images/open-air-buch.jpg" width="1024" height="690" alt="Buch über die Open-Air-Ausstellung in Stade"/>
         </a>
     </div>
 @stop
