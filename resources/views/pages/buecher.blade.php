917d17f74d1467ff6f3d076150362fed13e5c436
Dr. Tobias Quathamer <toddy@debian.org>
2021-12-13 12:55:26 +0100

    Books: Add open air, remove old links, add hugendubel and thalia

--- resources/views/pages/buecher.blade.php
+++ resources/views/pages/buecher.blade.php
@@ -3,16 +3,47 @@
 @section('content')
 <div class="row">
     <div class="col-xs-12 col-sm-6">
-        <a href="https://www.amazon.de/Cartoons-andere-Kostbarkeiten-Tetsche/dp/3830335164/">
+        <a href="https://www.kjm-buchverlag.de/produkt/tetsche-open-air/">
+            <img class="center-block img-responsive" src="{{ asset('images/buecher/open-air-stade.jpg') }}"
+                 alt="Tetsche Open Air" title="Tetsche Open Air" width="480" height="652" />
+        </a>
+    </div>
+    <div class="col-md-6">
+        <h2>Tetsche Open Air</h2>
+        <p>
+            Dokumentation über die Tetsche-Ausstellung in Stade an
+            den alten Kaimauern. Alles über das Making-Of, die Cartoons,
+            die Objekte und den alten Hafen. 88 Seiten im Hardcover!
+        </p>
+        <p>
+            KJM Buchverlag<br />
+            ISBN 978-3-96194-173-5
+        </p>
+        <p>
+            <a href="https://www.kjm-buchverlag.de/produkt/tetsche-open-air/" class="btn btn-danger">KJM Buchverlag</a>
+        </p>
+        <p>
+            <a href="https://www.hugendubel.de/de/buch_gebunden/tetsche_open_air-41677336-produkt-details.html" class="btn btn-danger">Bei Hugendubel ansehen</a>
+        </p>
+        <p>
+            <a href="https://www.thalia.de/shop/home/artikeldetails/A1062314305" class="btn btn-danger">Bei Thalia ansehen</a>
+        </p>
+        <p>
+            <a href="https://www.amazon.de/Tetsche-Open-Air-Cartoons-Objekte/dp/3961941734/" class="btn btn-danger">Bei Amazon ansehen</a>
+        </p>
+    </div>
+</div>
+<div class="row">
+    <div class="col-xs-12 col-sm-6">
+        <a href="https://www.carlsen.de/hardcover/cartoons-und-andere-kostbarkeiten/96575">
             <img class="center-block img-responsive" src="{{ asset('images/buecher/gesundheit.jpg') }}"
                  alt="Tetsche &ndash; Cartoons und andere Kostbarkeiten" title="Tetsche &ndash; Cartoons und andere Kostbarkeiten" width="480" height="595" />
         </a>
     </div>
     <div class="col-md-6">
-        <h2>Endlich!</h2>
         <h2>Cartoons &amp; andere Kostbarkeiten</h2>
         <p>
-            Das neue, große dicke Tetsche-Buch ist da!<br />
+            Das nächste große dicke Tetsche-Buch ist da!<br />
             Live und in Farbe, 208&nbsp;fette Seiten
         </p>
         <p>
@@ -20,22 +51,26 @@
             ISBN 978-3-8303-3516-0
         </p>
         <p>
-            <a href="https://www.amazon.de/Cartoons-andere-Kostbarkeiten-Tetsche/dp/3830335164/" class="btn btn-danger">Bei Amazon ansehen</a>
+            <a href="https://www.carlsen.de/hardcover/cartoons-und-andere-kostbarkeiten/96575" class="btn btn-danger">Carlsen-Verlag</a>
         </p>
         <p>
-            <a href="https://www.carlsen.de/hardcover/cartoons-und-andere-kostbarkeiten/96575" class="btn btn-danger">Lappan-Verlag</a>
+            <a href="https://www.hugendubel.de/de/buch_gebunden/tetsche-cartoons_und_andere_kostbarkeiten-32988855-produkt-details.html" class="btn btn-danger">Bei Hugendubel ansehen</a>
+        </p>
+        <p>
+            <a href="https://www.thalia.de/shop/home/artikeldetails/A1049087718" class="btn btn-danger">Bei Thalia ansehen</a>
+        </p>
+        <p>
+            <a href="https://www.amazon.de/Cartoons-andere-Kostbarkeiten-Tetsche/dp/3830335164/" class="btn btn-danger">Bei Amazon ansehen</a>
         </p>
     </div>
 </div>
 <div class="row">
     <div class="col-xs-12 col-sm-6">
-        <a href="https://www.amazon.de/Platsch-Fridos-Sprung-ins-Abenteuer/dp/3830312849/">
-            <img class="center-block img-responsive" src="{{ asset('images/buecher/platsch.jpg') }}"
-                 alt="Platsch! Fridos Sprung ins Abenteuer" title="Platsch! Fridos Sprung ins Abenteuer" width="480" height="588" />
-        </a>
+        <img class="center-block img-responsive" src="{{ asset('images/buecher/platsch.jpg') }}"
+             alt="Platsch! Fridos Sprung ins Abenteuer" title="Platsch! Fridos Sprung ins Abenteuer" width="480" height="588" />
     </div>
     <div class="col-md-6">
-        <h2>NEU!</h2>
+        <h2>Platsch! Fridos Sprung ins Abenteuer</h2>
         <p>
             Tetsches allererstes Kinderbuch ist da!<br/>
             Mit gereimten Versen von Hannes Lukas!
@@ -45,56 +80,37 @@
             zu der abenteuerlichen Geschichte von Frosch Frido und seinen Freunden
             Günni Gras und Willem Busch.<br/>
             Mit vielen lustigen Details zum Suchen und Entdecken.<br/>
-            Für Kinder von 3 &ndash; 8 Jahren
+            Für Kinder von 3 – 8 Jahren
         </p>
         <p>
             ISBN 978-3-8303-1284-0<br/>
             Lappan / Carlsen Verlag<br/>
-            € 12,99
-        </p>
-        <p>
-            Ab sofort in jeder Buchhandlung erhältlich.
-        </p>
-        <p>
-            <a href="https://www.amazon.de/Platsch-Fridos-Sprung-ins-Abenteuer/dp/3830312849/" class="btn btn-danger">Bei Amazon ansehen</a>
         </p>
     </div>
 </div>
 <div class="row">
     <div class="col-xs-12 col-sm-6">
-        <a href="https://www.amazon.de/Hoffentlich-ist-es-nichts-Ernstes/dp/3830332459">
-            <img class="center-block img-responsive" src="{{ asset('images/buecher/hoffentlich-nichts-ernstes.jpg') }}"
-                 alt="Hoffentlich ist es nichts Ernstes!" title="Hoffentlich ist es nichts Ernstes!" width="480" height="581" />
-        </a>
+        <img class="center-block img-responsive" src="{{ asset('images/buecher/hoffentlich-nichts-ernstes.jpg') }}"
+             alt="Hoffentlich ist es nichts Ernstes!" title="Hoffentlich ist es nichts Ernstes!" width="480" height="581" />
     </div>
     <div class="col-md-6">
         <h2>Hoffentlich ist es nichts Ernstes!</h2>
         <p>
             ISBN 978-3-8303-3245-9<br/>
             Lappan / Carlsen Verlag<br/>
-            € 19,95
-        </p>
-        <p>
-            <a href="https://www.amazon.de/Hoffentlich-ist-es-nichts-Ernstes/dp/3830332459" class="btn btn-danger">Bei Amazon ansehen</a>
         </p>
     </div>
 </div>
 <div class="row">
     <div class="col-xs-12 col-sm-6">
-        <a href="https://www.amazon.de/Beste-Bilder-8-Deutscher-Cartoonpreis/dp/3830334591">
-            <img class="center-block img-responsive" src="{{ asset('images/buecher/beste-bilder.jpg') }}"
-                 alt="Beste Bilder 8 (Deutscher Cartoonpreis)" title="Beste Bilder 8 (Deutscher Cartoonpreis)" width="480" height="581" />
-        </a>
+        <img class="center-block img-responsive" src="{{ asset('images/buecher/beste-bilder.jpg') }}"
+             alt="Beste Bilder 8 (Deutscher Cartoonpreis)" title="Beste Bilder 8 (Deutscher Cartoonpreis)" width="480" height="581" />
     </div>
     <div class="col-md-6">
         <h2>Beste Bilder 8 (Deutscher Cartoonpreis)</h2>
         <p>
             ISBN 978-3-8303-3459-0<br/>
             Lappan / Carlsen Verlag<br/>
-            € 12,00
-        </p>
-        <p>
-            <a href="https://www.amazon.de/Beste-Bilder-8-Deutscher-Cartoonpreis/dp/3830334591" class="btn btn-danger">Bei Amazon ansehen</a>
         </p>
     </div>
 </div>
