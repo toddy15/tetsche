1d03cefe9e7d4a2b2c0734a666634e2639d31e67
Dr. Tobias Quathamer <toddy@debian.org>
2022-03-12 21:57:24 +0100

    Update user model

--- config/app.php
+++ config/app.php
@@ -137,6 +137,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
+        Illuminate\Notifications\NotificationServiceProvider::class,
 
         /*
          * Application Service Providers...
