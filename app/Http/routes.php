371e0abc06092b4d81f3912ba712a7eb012569e7
Dr. Tobias Quathamer <toddy@debian.org>
2022-03-12 22:12:22 +0100

    GET logout

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,6 +2,8 @@
 
 // Authentication routes...
 Auth::routes();
+Route::get('/logout', 'Auth\LoginController@logout');
+
 //Route::get('auth/login', 'Auth\AuthController@getLogin');
 //Route::post('auth/login', 'Auth\AuthController@postLogin');
 //Route::get('auth/logout', 'Auth\AuthController@getLogout');
@@ -13,12 +15,12 @@ Auth::routes();
 //Route::post('auth/register', ['middleware' => 'auth', 'uses' => 'Auth\AuthController@postRegister']);
 
 // Password reset link request routes...
-Route::get('password/email', 'Auth\PasswordController@getEmail');
-Route::post('password/email', 'Auth\PasswordController@postEmail');
+//Route::get('password/email', 'Auth\PasswordController@getEmail');
+//Route::post('password/email', 'Auth\PasswordController@postEmail');
 
 // Password reset routes...
-Route::get('password/reset/{token}', 'Auth\PasswordController@getReset');
-Route::post('password/reset', 'Auth\PasswordController@postReset');
+//Route::get('password/reset/{token}', 'Auth\PasswordController@getReset');
+//Route::post('password/reset', 'Auth\PasswordController@postReset');
 
 
 
