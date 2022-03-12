f558bad12212cb49084651e0c3632680c0e0e8bf
Dr. Tobias Quathamer <toddy@debian.org>
2022-03-12 21:55:25 +0100

    Convert auth controllers

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,9 +1,10 @@
 <?php
 
 // Authentication routes...
-Route::get('auth/login', 'Auth\AuthController@getLogin');
-Route::post('auth/login', 'Auth\AuthController@postLogin');
-Route::get('auth/logout', 'Auth\AuthController@getLogout');
+Auth::routes();
+//Route::get('auth/login', 'Auth\AuthController@getLogin');
+//Route::post('auth/login', 'Auth\AuthController@postLogin');
+//Route::get('auth/logout', 'Auth\AuthController@getLogout');
 
 // Registration routes...
 //Route::get('auth/register', 'Auth\AuthController@getRegister');
