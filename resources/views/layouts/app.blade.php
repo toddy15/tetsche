7d3835f368f2d37c625a0eb5b71ff8282d134be4
Dr. Tobias Quathamer <toddy@debian.org>
2022-03-14 20:49:31 +0100

    More layout updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="de">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -7,7 +7,19 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <meta name="author" content="Tetsche">
+    @if (isset($description))
+        <meta name="description" lang="de" content="{{ $description }}">
+    @endif
+    <title>{{ isset($title) ? $title . ' | ': '' }}Tetsche</title>
+
+    <link rel="canonical" href="{!! urldecode(Request::url()) !!}"/>
+    <link rel="shortcut icon" href="{{ asset('theme/images/favicon.ico') }}">
+    <link rel="icon" sizes="192x192" href="{{ asset('theme/images/puempel-192x192.png') }}">
+    @foreach ([180, 152, 144, 120, 114, 76, 72, 60, 57] as $res)
+        <link rel="apple-touch-icon" sizes="{{ $res }}x{{ $res }}"
+              href="{{ asset('theme/images/puempel-' . $res . 'x' . $res . '.png') }}">
+    @endforeach
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -21,63 +33,40 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+        @include('partials.nav')
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
+        <div class="container-xl">
+            @include('partials.errors')
+            @include('partials.info')
 
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
+            <main class="py-4">
+                @yield('content')
+            </main>
+        </div>
     </div>
+
+    @include('additional_javascript')
+    <!-- Matomo -->
+    <script type="text/javascript">
+        var _paq = window._paq = window._paq || [];
+        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        (function () {
+            var u = "https://www.tetsche.de/matomo/";
+            _paq.push(['setTrackerUrl', u + 'matomo.php']);
+            _paq.push(['setSiteId', '1']);
+            var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+            g.type = 'text/javascript';
+            g.async = true;
+            g.src = u + 'matomo.js';
+            s.parentNode.insertBefore(g, s);
+        })();
+    </script>
+    <noscript>
+        <img referrerpolicy="no-referrer-when-downgrade"
+             src="https://www.tetsche.de/matomo/matomo.php?idsite=1&amp;rec=1" style="border:0" alt=""/>
+    </noscript>
+    <!-- End Matomo Code -->
 </body>
 </html>
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,34 +1,88 @@
-<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #fa6666;">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="{{ url('/') }}">Home</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item {{ Request::is('tetsche') ? 'active' : ''}}">
-                    <a class="nav-link {{ Request::is('tetsche') ? 'active' : '' }}" {!! Request::is('tetsche') ? 'aria-current="page"' : '' !!} href="{{ url('tetsche') }}">Tetsche</a>
-                </li>
-                <li class="nav-item {{ Request::is('cartoon') ? 'active' : ''}}">
-                    <a class="nav-link {{ Request::is('cartoon') ? 'active' : '' }}" {!! Request::is('cartoon') ? 'aria-current="page"' : '' !!} href="{{ url('cartoon') }}">Cartoon</a>
-                </li>
-                <li class="nav-item {{ Request::is('archiv') ? 'active' : ''}}">
-                    <a class="nav-link {{ Request::is('archiv') ? 'active' : '' }}" {!! Request::is('archiv') ? 'aria-current="page"' : '' !!} href="{{ url('archiv') }}">Archiv</a>
-                </li>
-                <li class="nav-item {{ Request::is('bücher') ? 'active' : ''}}">
-                    <a class="nav-link {{ Request::is('bücher') ? 'active' : '' }}" {!! Request::is('bücher') ? 'aria-current="page"' : '' !!} href="{{ url('bücher') }}">Bücher</a>
-                </li>
-                <li class="nav-item {{ Request::is('gästebuch') ? 'active' : ''}}">
-                    <a class="nav-link {{ Request::is('gästebuch') ? 'active' : '' }}" {!! Request::is('gästebuch') ? 'aria-current="page"' : '' !!} href="{{ url('gästebuch') }}">Gästebuch</a>
-                </li>
-                <li class="nav-item {{ Request::is('impressum') ? 'active' : ''}}">
-                    <a class="nav-link {{ Request::is('impressum') ? 'active' : '' }}" {!! Request::is('impressum') ? 'aria-current="page"' : '' !!} href="{{ url('impressum') }}">Impressum</a>
-                </li>
-                <li class="nav-item {{ Request::is('datenschutzerklärung') ? 'active' : ''}}">
-                    <a class="nav-link {{ Request::is('datenschutzerklärung') ? 'active' : '' }}" {!! Request::is('datenschutzerklärung') ? 'aria-current="page"' : '' !!} href="{{ url('datenschutzerklärung') }}">Datenschutzerklärung</a>
-                </li>
+        <nav class="navbar navbar-expand-md navbar-light bg-danger shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    Home
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav me-auto">
+                        <li class="nav-item {{ Request::is('tetsche') ? 'active' : ''}}">
+                            <a class="nav-link {{ Request::is('tetsche') ? 'active' : '' }}"
+                               {!! Request::is('tetsche') ? 'aria-current="page"' : '' !!} href="{{ url('tetsche') }}">Tetsche</a>
+                        </li>
+                        <li class="nav-item {{ Request::is('cartoon') ? 'active' : ''}}">
+                            <a class="nav-link {{ Request::is('cartoon') ? 'active' : '' }}"
+                               {!! Request::is('cartoon') ? 'aria-current="page"' : '' !!} href="{{ url('cartoon') }}">Cartoon</a>
+                        </li>
+                        <li class="nav-item {{ Request::is('archiv') ? 'active' : ''}}">
+                            <a class="nav-link {{ Request::is('archiv') ? 'active' : '' }}"
+                               {!! Request::is('archiv') ? 'aria-current="page"' : '' !!} href="{{ url('archiv') }}">Archiv</a>
+                        </li>
+                        <li class="nav-item {{ Request::is('bücher') ? 'active' : ''}}">
+                            <a class="nav-link {{ Request::is('bücher') ? 'active' : '' }}"
+                               {!! Request::is('bücher') ? 'aria-current="page"' : '' !!} href="{{ url('bücher') }}">Bücher</a>
+                        </li>
+                        <li class="nav-item {{ Request::is('gästebuch') ? 'active' : ''}}">
+                            <a class="nav-link {{ Request::is('gästebuch') ? 'active' : '' }}"
+                               {!! Request::is('gästebuch') ? 'aria-current="page"' : '' !!} href="{{ url('gästebuch') }}">Gästebuch</a>
+                        </li>
+                        <li class="nav-item {{ Request::is('impressum') ? 'active' : ''}}">
+                            <a class="nav-link {{ Request::is('impressum') ? 'active' : '' }}"
+                               {!! Request::is('impressum') ? 'aria-current="page"' : '' !!} href="{{ url('impressum') }}">Impressum</a>
+                        </li>
+                        <li class="nav-item {{ Request::is('datenschutzerklärung') ? 'active' : ''}}">
+                            <a class="nav-link {{ Request::is('datenschutzerklärung') ? 'active' : '' }}"
+                               {!! Request::is('datenschutzerklärung') ? 'aria-current="page"' : '' !!} href="{{ url('datenschutzerklärung') }}">Datenschutzerklärung</a>
+                        </li>
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ms-auto">
+                        <!-- Authentication Links -->
+                        {{--
+                        @guest
+                            @if (Route::has('login'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                </li>
+                            @endif
+
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                        --}}
+                        @unless(Auth::guest())
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endunless
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        {{--
                 @unless (Auth::guest())
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
@@ -51,3 +105,4 @@
         </div>
     </div>
 </nav>
+        --}}
