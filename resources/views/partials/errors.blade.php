fc09b6718a23925316c7b11ee90dd8287fc6baea
Dr. Tobias Quathamer <toddy@debian.org>
2022-03-14 21:55:10 +0100

    Update alerts to bootstrap v5

--- resources/views/partials/errors.blade.php
+++ resources/views/partials/errors.blade.php
@@ -1,11 +1,11 @@
 @if ($errors->any())
-    <div class="alert alert-danger alert-dismissible" role="alert">
-        <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Schließen</span></button>
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
         <strong>Huch!</strong> Es gibt fehlerhafte Eingaben:<br><br>
         <ul>
             @foreach($errors->all() as $error)
                 <li>{{ $error }}</li>
             @endforeach
         </ul>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Schließen"></button>
     </div>
 @endif
--- resources/views/partials/info.blade.php
+++ resources/views/partials/info.blade.php
@@ -1,12 +1,12 @@
 @if (Session::has('error'))
-    <div class="alert alert-danger alert-dismissible" role="alert">
-        <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
         {{ Session::get('error') }}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Schließen"></button>
     </div>
 @endif
 @if (Session::has('info'))
-    <div class="alert alert-info alert-dismissible" role="alert">
-        <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+    <div class="alert alert-info alert-dismissible fade show" role="alert">
         {{ Session::get('info') }}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Schließen"></button>
     </div>
 @endif
