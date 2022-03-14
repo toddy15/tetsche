c53ad98f6a22424c2dcdedfc492b8d03db3c71cc
Dr. Tobias Quathamer <toddy@debian.org>
2022-03-14 16:04:18 +0100

    Add mailable for guestbook post

---
+++ app/Mail/NewGuestbookPost.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Mail;
+
+use App\GuestbookPost;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class NewGuestbookPost extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public GuestbookPost $guestbook_post;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(GuestbookPost $guestbook_post)
+    {
+        $this->guestbook_post = $guestbook_post;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->text('emails.guestbook');
+    }
+}
