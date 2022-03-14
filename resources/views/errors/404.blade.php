67ce89e315a492017e59a3fc369003ec219ccf4e
Dr. Tobias Quathamer <toddy@debian.org>
2022-03-14 14:37:42 +0100

    Rename 404 template

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -3,8 +3,5 @@
 @section('content')
     <div class="jumbotron alert-danger">
         <h1>Seite nicht vorhanden</h1>
-        @foreach ($msg_lines as $msg_line)
-            <p>{{ $msg_line }}</p>
-        @endforeach
     </div>
 @stop
