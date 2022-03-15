e517a426167eca4c88f947a65217385540ee9607
Dr. Tobias Quathamer <toddy@debian.org>
2022-03-15 21:27:33 +0100

    Use anonymous classes for migrations

--- database/migrations/2015_06_20_160641_create_guestbook_posts_table.php
+++ database/migrations/2015_06_20_160641_create_guestbook_posts_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateGuestbookPostsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -31,4 +31,4 @@ class CreateGuestbookPostsTable extends Migration
     {
         Schema::drop('guestbook_posts');
     }
-}
+};
--- database/migrations/2018_12_23_003021_create_publication_dates_table.php
+++ database/migrations/2018_12_23_003021_create_publication_dates_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreatePublicationDatesTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -29,4 +29,4 @@ class CreatePublicationDatesTable extends Migration
     {
         Schema::drop('publication_dates');
     }
-}
+};
