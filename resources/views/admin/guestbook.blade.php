1915073eeb20b48fe7c4229bbbabb894dc9f9138
Dr. Tobias Quathamer <toddy@debian.org>
2022-03-16 20:43:33 +0100

    Update guestbook overview

--- resources/views/admin/guestbook.blade.php
+++ resources/views/admin/guestbook.blade.php
@@ -6,14 +6,22 @@
         Hier können Beiträge angezeigt werden, die eine entsprechende Zuordnung haben.
     </p>
 
-    <div class="row">
-        <div class="col-4">
-            <a href="{{ action([App\Http\Controllers\SpamController::class, 'showPosts'], ['manual_ham']) }}" class="btn btn-primary btn-block">Manuell als Ham gelernt</a>
-            <a href="{{ action([App\Http\Controllers\SpamController::class, 'showPosts'], ['unsure']) }}" class="btn btn-primary btn-block">Keine Zuordnung</a>
-            <a href="{{ action([App\Http\Controllers\SpamController::class, 'showPosts'], ['manual_spam']) }}" class="btn btn-primary btn-block">Manuell als Spam gelernt</a>
-            <a href="{{ action([App\Http\Controllers\SpamController::class, 'showPosts'], ['autolearn_ham']) }}" class="btn btn-primary btn-block">Automatisch als Ham gelernt</a>
-            <a href="{{ action([App\Http\Controllers\SpamController::class, 'showPosts'], ['autolearn_spam']) }}" class="btn btn-primary btn-block">Automatisch als Spam gelernt</a>
-        </div>
+    <div class="list-group">
+        <a class="list-group-item list-group-item-action"
+           href="{{ action([App\Http\Controllers\SpamController::class, 'showPosts'], ['manual_ham']) }}">Manuell als
+            Ham gelernt</a>
+        <a class="list-group-item list-group-item-action"
+           href="{{ action([App\Http\Controllers\SpamController::class, 'showPosts'], ['unsure']) }}">Keine
+            Zuordnung</a>
+        <a class="list-group-item list-group-item-action"
+           href="{{ action([App\Http\Controllers\SpamController::class, 'showPosts'], ['manual_spam']) }}">Manuell als
+            Spam gelernt</a>
+        <a class="list-group-item list-group-item-action"
+           href="{{ action([App\Http\Controllers\SpamController::class, 'showPosts'], ['autolearn_ham']) }}">Automatisch
+            als Ham gelernt</a>
+        <a class="list-group-item list-group-item-action"
+           href="{{ action([App\Http\Controllers\SpamController::class, 'showPosts'], ['autolearn_spam']) }}">Automatisch
+            als Spam gelernt</a>
     </div>
     {{--
     @TODO: The relearning has not work correctly for ages.
