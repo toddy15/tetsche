8455cf50f52fee663a10d4f4884bd69087bc3006
Dr. Tobias Quathamer <toddy@debian.org>
2022-03-20 23:49:28 +0100

    Update matomo tracking code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,25 +43,24 @@
 </div>
 
 <!-- Matomo -->
-<script type="text/javascript">
+<script>
     var _paq = window._paq = window._paq || [];
     /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
     _paq.push(['trackPageView']);
     _paq.push(['enableLinkTracking']);
     (function () {
-        var u = "https://www.tetsche.de/matomo/";
+        var u = "https://matomo.tetsche.de/";
         _paq.push(['setTrackerUrl', u + 'matomo.php']);
         _paq.push(['setSiteId', '1']);
         var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
-        g.type = 'text/javascript';
         g.async = true;
         g.src = u + 'matomo.js';
         s.parentNode.insertBefore(g, s);
     })();
 </script>
 <noscript>
-    <img referrerpolicy="no-referrer-when-downgrade"
-         src="https://www.tetsche.de/matomo/matomo.php?idsite=1&amp;rec=1" style="border:0" alt=""/>
+    <img referrerpolicy="no-referrer-when-downgrade" src="https://matomo.tetsche.de/matomo.php?idsite=1&amp;rec=1"
+         style="border:0" alt=""/>
 </noscript>
 <!-- End Matomo Code -->
 </body>
