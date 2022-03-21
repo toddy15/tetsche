439ac35f77f96364506c75692e0d00a689eb575a
Dr. Tobias Quathamer <toddy@debian.org>
2022-03-21 14:12:52 +0100

    Do not rewrite URLs in development

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,5 +14,8 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .copy('resources/images/favicons/*', 'public')
+    .options({
+        processCssUrls: mix.inProduction(),
+    })
     .sourceMaps()
     .version();
