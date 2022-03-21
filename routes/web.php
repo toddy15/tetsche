9a10a28675025b05e317c9a6fc3b61931350db0e
Dr. Tobias Quathamer <toddy@debian.org>
2022-03-21 21:56:45 +0100

    Update some HTTP codes

--- routes/web.php
+++ routes/web.php
@@ -7,17 +7,6 @@ use App\Http\Middleware\Authenticate;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 // Static pages
 Route::view('/', 'pages.homepage', [
     'description' => 'Tetsche-Website',
@@ -70,13 +59,12 @@ Route::middleware(Authenticate::class)->group(function () {
 
     Route::get('/cartoons', [CartoonsController::class, 'index']);
     Route::get('/cartoons/forceNewCartoon', [CartoonsController::class, 'forceNewCartoon']);
-//    Route::get('/cartoons/{id}/edit', [CartoonsController::class, 'edit'])
-//        ->where('id', '[0-9]+');
-//    Route::put('/cartoons/{id}', [CartoonsController::class, 'update'])
-//        ->where('id', '[0-9]+');
-//    Route::delete('/cartoons/{id}', [CartoonsController::class, 'destroy'])
-//        ->where('id', '[0-9]+');
 });
 
 // Close registration
 Auth::routes(['register' => false]);
+
+// Redirect routes
+Route::permanentRedirect('/gästebuch', '/gaestebuch');
+Route::permanentRedirect('/bücher', '/buecher');
+Route::permanentRedirect('/auth/login', '/login');
