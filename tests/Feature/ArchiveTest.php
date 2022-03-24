97197b8c8084ba6c4d86f132ecd46868790a35e6
Dr. Tobias Quathamer <toddy@debian.org>
2022-03-24 11:46:11 +0100

    Rewrite GuestbookTest for pestphp

--- tests/Feature/ArchiveTest.php
+++ tests/Feature/ArchiveTest.php
@@ -4,10 +4,34 @@ use App\Models\PublicationDate;
 
 use function Pest\Laravel\get;
 
-test('a guest can view the archive', function () {
-    // Ensure there are some PublicationDates
-    PublicationDate::factory()->count(20)->create();
+beforeEach(function () {
+    $publicationDates = [
+        '2021-11-25',
+        '2021-12-02',
+        '2021-12-09',
+        '2021-12-16',
+        '2021-12-23',
+        '2021-12-30',
+        '2022-01-06',
+        '2022-01-13',
+        '2022-01-20',
+        '2022-01-27',
+        '2022-02-03',
+        '2022-02-10',
+        '2022-02-17',
+        '2022-02-24',
+        '2022-03-03',
+        '2022-03-10',
+        '2022-03-17',
+        '2022-03-24',
+    ];
 
+    foreach ($publicationDates as $publicationDate) {
+        PublicationDate::factory()->create(['publish_on' => $publicationDate]);
+    }
+});
+
+test('a guest can view the archive', function () {
     // @TODO: check archive with created dates above
     get('/archiv')
         ->assertOk()
---
+++ tests/Feature/GuestbookTest.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Models\GuestbookPost;
+use App\Models\PublicationDate;
+
+use function Pest\Laravel\get;
+use function Pest\Laravel\post;
+
+test('a guest can view the guestbook page', function () {
+    // Ensure there are entries
+    GuestbookPost::factory()->count(5)->create();
+
+    get(route('gaestebuch.index'))
+        ->assertOk()
+        ->assertSeeText('Gästebuch')
+        ->assertSeeText('Name')
+        ->assertSeeText('Nachricht');
+});
+
+test('a guest can post a new entry', function () {
+    // Ensure there are entries
+    GuestbookPost::factory()->count(5)->create();
+
+    // There has to be at least one PublicationDate for the rebus spamcheck.
+    PublicationDate::factory()->create();
+
+    get(route('gaestebuch.create'))
+        ->assertOk()
+        ->assertSeeText('Gästebuch: Neuer Eintrag');
+
+    $entry = GuestbookPost::factory()->raw([
+        'cheffe' => null,
+        'category' => 'unsure',
+        'spam_detection' => 'IP: 127.0.0.1, Browser: Symfony',
+    ]);
+    $this->assertDatabaseMissing('guestbook_posts', $entry);
+
+    post(route('gaestebuch.store'), $entry)
+        ->assertRedirect();
+
+    $this->assertDatabaseHas('guestbook_posts', $entry);
+
+    get(route('gaestebuch.index'))
+        ->assertSeeText($entry['name'])
+        ->assertSeeText($entry['message']);
+});
