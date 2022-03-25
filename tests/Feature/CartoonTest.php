51463d312103f370c8c64d8226ea22d7399d1313
Dr. Tobias Quathamer <toddy@debian.org>
2022-03-25 13:33:02 +0100

    Add new test for cartoon

--- tests/Feature/CartoonTest.php
+++ tests/Feature/CartoonTest.php
@@ -46,3 +46,26 @@ test('a guest cannot view a future cartoon', function () {
         ->assertSeeText('Die Rebus-Abbildungen ergeben zusammen einen neuen Begriff.')
         ->assertSeeText('Auflösung nächste Woche');
 });
+
+test('a guest can view the next cartoon at the correct time', function () {
+    // Ensure there are PublicationDates
+    PublicationDate::factory()->create(['publish_on' => '2022-03-11']);
+    PublicationDate::factory()->create(['publish_on' => '2022-03-17']);
+    PublicationDate::factory()->create(['publish_on' => '2022-03-24']);
+
+    testTime()->freeze('2022-03-23 17:59:59');
+
+    get('/cartoon')
+        ->assertOk()
+        ->assertSeeText('Cartoon der Woche . . . vom 17. März 2022')
+        ->assertSeeText('Die Rebus-Abbildungen ergeben zusammen einen neuen Begriff.')
+        ->assertSeeText('Auflösung nächste Woche');
+
+    testTime()->freeze('2022-03-23 18:00:00');
+
+    get('/cartoon')
+        ->assertOk()
+        ->assertSeeText('Cartoon der Woche . . . vom 24. März 2022')
+        ->assertSeeText('Die Rebus-Abbildungen ergeben zusammen einen neuen Begriff.')
+        ->assertSeeText('Auflösung nächste Woche');
+});
