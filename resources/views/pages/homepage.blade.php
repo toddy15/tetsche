16fcdd2636aea4fcfdbc93a9b3c1af4ead4ba915
Dr. Tobias Quathamer <toddy@debian.org>
2022-10-31 20:10:05 +0100

    Add initial white dog

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -4,7 +4,7 @@
     <h1 class="display-1 mb-5">Tetsche-Website</h1>
 
     <div class="text-center">
-        <img class="img-fluid" src="{{ asset('images/hund-grinsend.jpg') }}" alt="Grinsender Hund" width="552"
+        <img class="img-fluid" src="{{ asset('images/hund-grinsend-weiss.jpg') }}" alt="Grinsender Hund" width="552"
              height="361" />
     </div>
 @stop
