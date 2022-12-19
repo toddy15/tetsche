2be47274de45035a95ebd6bb5f8c22713c9b041b
Dr. Tobias Quathamer <toddy@debian.org>
2022-12-19 12:31:30 +0100

    Update language files

--- lang/de/auth.php
+++ lang/de/auth.php
@@ -1,5 +1,20 @@
 <?php
 
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
     'failed' => 'Die E-Mail-Adresse oder das Passwort sind falsch.',
+    'password' => 'Das angegebene Passwort ist falsch.',
+    'throttle' => 'Zu viele Anmeldeversuche. Bitte nach :seconds Sekunden erneut probieren.',
+
 ];
