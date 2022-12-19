374791cd99cce5b4a2b8238d1c876497ee83955e
Dr. Tobias Quathamer <toddy@debian.org>
2022-12-19 15:23:35 +0100

    Add strict types to tests

--- tests/Feature/CartoonTest.php
+++ tests/Feature/CartoonTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use App\Models\PublicationDate;
 use function Pest\Laravel\get;
 use function Spatie\PestPluginTestTime\testTime;
--- tests/Feature/PublicationDateTest.php
+++ tests/Feature/PublicationDateTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use App\Models\PublicationDate;
 use App\Models\User;
 use function Pest\Laravel\actingAs;
