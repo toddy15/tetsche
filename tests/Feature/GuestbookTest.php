e81a1b5110c9e1500cb67b32194e5e7c79f3e54b
Dr. Tobias Quathamer <toddy@debian.org>
2023-02-01 12:14:45 +0100

    Set up PublicationDates to ensure a valid test

--- tests/Feature/GuestbookTest.php
+++ tests/Feature/GuestbookTest.php
@@ -6,6 +6,38 @@ use App\Models\GuestbookPost;
 use App\Models\PublicationDate;
 use function Pest\Laravel\get;
 use function Pest\Laravel\post;
+use function Spatie\PestPluginTestTime\testTime;
+
+beforeEach(function () {
+    $dates = [
+        '2021-11-18',
+        '2021-11-25', // not available anymore
+        '2021-12-02', // oldest date in the archive
+        '2021-12-09',
+        '2021-12-16',
+        '2021-12-23',
+        '2021-12-30',
+        '2022-01-06',
+        '2022-01-13',
+        '2022-01-20',
+        '2022-01-27',
+        '2022-02-03',
+        '2022-02-10',
+        '2022-02-17',
+        '2022-02-24',
+        '2022-03-03',
+        '2022-03-10',
+        '2022-03-17', // newest date in the archive
+        '2022-03-24', // current
+        '2022-03-31', // future cartoon
+    ];
+
+    foreach ($dates as $date) {
+        PublicationDate::factory()->create(['publish_on' => $date]);
+    }
+
+    testTime()->freeze('2022-03-26 14:30:00');
+});
 
 test('a guest can view the guestbook page', function () {
     // Ensure there are entries
@@ -25,9 +57,6 @@ test('a guest can post a new entry', function () {
     GuestbookPost::factory()
         ->count(5)
         ->create();
-
-    // There has to be at least one PublicationDate for the rebus spamcheck.
-    PublicationDate::factory()->create();
 
     get(route('gaestebuch.create'))
         ->assertOk()
