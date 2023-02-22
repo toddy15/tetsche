dc981017662fb5f691a55ed182405dac6d433c11
Dr. Tobias Quathamer <toddy@debian.org>
2023-02-22 16:05:53 +0100

    Run pint

--- tests/Services/SpamfilterTest.php
+++ tests/Services/SpamfilterTest.php
@@ -4,27 +4,27 @@ declare(strict_types=1);
 
 use App\Services\Spamfilter;
 
-it("can get a spamfilter instance", function () {
+it('can get a spamfilter instance', function () {
     $s = new Spamfilter();
     expect($s)->toBeInstanceOf(Spamfilter::class);
 });
 
-it("can parse a text into tokens", function () {
+it('can parse a text into tokens', function () {
     $s = new Spamfilter();
 
-    $result = $s->parse("Those are words");
+    $result = $s->parse('Those are words');
     expect($result)->toBe([
         'Those' => 1,
-        'are'   => 1,
+        'are' => 1,
         'words' => 1,
     ]);
 
     $result = $s->parse("Those\nare\nmore and more words");
     expect($result)->toBe([
         'Those' => 1,
-        'are'   => 1,
-        'more'   => 1,
-        'and'   => 1,
+        'are' => 1,
+        'more' => 1,
+        'and' => 1,
         'words' => 1,
     ]);
 });
