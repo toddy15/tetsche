253cbbc4503a9bcd87595a5c0b3970a36b071c2f
Dr. Tobias Quathamer <toddy@debian.org>
2023-02-26 19:57:15 +0100

    Update CartoonTest

--- tests/Feature/CartoonTest.php
+++ tests/Feature/CartoonTest.php
@@ -6,15 +6,40 @@ use App\Models\PublicationDate;
 use function Pest\Laravel\get;
 use function Spatie\PestPluginTestTime\testTime;
 
+beforeEach(function () {
+    $dates = [
+        '2021-11-25', // not available anymore
+        '2021-12-02', // oldest date in the archive
+        '2021-12-09',
+        '2021-12-16',
+        '2021-12-23',
+        '2021-12-30',
+        '2022-01-06',
+        '2022-01-13',
+        '2022-01-20',
+        '2022-01-27',
+        '2022-02-03',
+        '2022-02-10',
+        '2022-02-17',
+        '2022-02-24',
+        '2022-03-03',
+        '2022-03-10',
+        '2022-03-17', // newest date in the archive
+        '2022-03-24', // current
+        '2022-03-31', // future cartoon
+    ];
+
+    foreach ($dates as $date) {
+        PublicationDate::factory()->create(['publish_on' => $date]);
+    }
+
+    testTime()->freeze('2022-03-26 14:30:00');
+});
+
 test('a guest can view the current cartoon', function () {
-    testTime()->freeze('2022-03-14 14:30:00');
-
-    // Ensure there is a PublicationDate
-    PublicationDate::factory()->create(['publish_on' => '2022-03-11']);
-
     get('/cartoon')
         ->assertOk()
-        ->assertSeeText('Cartoon der Woche . . . vom 11. März 2022')
+        ->assertSeeText('Cartoon der Woche . . . vom 24. März 2022')
         ->assertSeeText(
             'Die Rebus-Abbildungen ergeben zusammen einen neuen Begriff.',
         )
@@ -22,32 +47,11 @@ test('a guest can view the current cartoon', function () {
 });
 
 test('a guest can view the next current cartoon', function () {
-    testTime()->freeze('2022-03-20 14:30:00');
-
-    // Ensure there are PublicationDates
-    PublicationDate::factory()->create(['publish_on' => '2022-03-11']);
-    PublicationDate::factory()->create(['publish_on' => '2022-03-17']);
+    testTime()->freeze('2022-04-01 14:30:00');
 
     get('/cartoon')
         ->assertOk()
-        ->assertSeeText('Cartoon der Woche . . . vom 17. März 2022')
-        ->assertSeeText(
-            'Die Rebus-Abbildungen ergeben zusammen einen neuen Begriff.',
-        )
-        ->assertSeeText('Auflösung nächste Woche');
-});
-
-test('a guest cannot view a future cartoon', function () {
-    testTime()->freeze('2022-03-22 14:30:00');
-
-    // Ensure there are PublicationDates
-    PublicationDate::factory()->create(['publish_on' => '2022-03-11']);
-    PublicationDate::factory()->create(['publish_on' => '2022-03-17']);
-    PublicationDate::factory()->create(['publish_on' => '2022-03-24']);
-
-    get('/cartoon')
-        ->assertOk()
-        ->assertSeeText('Cartoon der Woche . . . vom 17. März 2022')
+        ->assertSeeText('Cartoon der Woche . . . vom 31. März 2022')
         ->assertSeeText(
             'Die Rebus-Abbildungen ergeben zusammen einen neuen Begriff.',
         )
@@ -55,26 +59,21 @@ test('a guest cannot view a future cartoon', function () {
 });
 
 test('a guest can view the next cartoon at the correct time', function () {
-    // Ensure there are PublicationDates
-    PublicationDate::factory()->create(['publish_on' => '2022-03-11']);
-    PublicationDate::factory()->create(['publish_on' => '2022-03-17']);
-    PublicationDate::factory()->create(['publish_on' => '2022-03-24']);
-
-    testTime()->freeze('2022-03-23 17:59:59');
+    testTime()->freeze('2022-03-30 17:59:59');
 
     get('/cartoon')
         ->assertOk()
-        ->assertSeeText('Cartoon der Woche . . . vom 17. März 2022')
+        ->assertSeeText('Cartoon der Woche . . . vom 24. März 2022')
         ->assertSeeText(
             'Die Rebus-Abbildungen ergeben zusammen einen neuen Begriff.',
         )
         ->assertSeeText('Auflösung nächste Woche');
 
-    testTime()->freeze('2022-03-23 18:00:00');
+    testTime()->freeze('2022-03-30 18:00:00');
 
     get('/cartoon')
         ->assertOk()
-        ->assertSeeText('Cartoon der Woche . . . vom 24. März 2022')
+        ->assertSeeText('Cartoon der Woche . . . vom 31. März 2022')
         ->assertSeeText(
             'Die Rebus-Abbildungen ergeben zusammen einen neuen Begriff.',
         )
