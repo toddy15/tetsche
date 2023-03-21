c3c7c27718ddaa0151453b484b57959e80d89cb1
Dr. Tobias Quathamer <toddy@debian.org>
2023-03-21 23:10:51 +0100

    Move PublicationDates creation into Pest.php

--- tests/Feature/GuestbookTest.php
+++ tests/Feature/GuestbookTest.php
@@ -3,45 +3,12 @@
 declare(strict_types=1);
 
 use App\Models\GuestbookPost;
-use App\Models\PublicationDate;
 use App\Models\User;
-use Carbon\Carbon;
 use function Pest\Laravel\actingAs;
 use function Pest\Laravel\delete;
 use function Pest\Laravel\get;
 use function Pest\Laravel\post;
 use function Pest\Laravel\put;
-
-beforeEach(function () {
-    $dates = [
-        '2021-11-18',
-        '2021-11-25', // not available anymore
-        '2021-12-02', // oldest date in the archive
-        '2021-12-09',
-        '2021-12-16',
-        '2021-12-23',
-        '2021-12-30',
-        '2022-01-06',
-        '2022-01-13',
-        '2022-01-20',
-        '2022-01-27',
-        '2022-02-03',
-        '2022-02-10',
-        '2022-02-17',
-        '2022-02-24',
-        '2022-03-03',
-        '2022-03-10',
-        '2022-03-17', // newest date in the archive
-        '2022-03-24', // current
-        '2022-03-31', // future cartoon
-    ];
-
-    foreach ($dates as $date) {
-        PublicationDate::factory()->create(['publish_on' => $date]);
-    }
-
-    Carbon::setTestNow('2022-03-26 14:30:00');
-});
 
 test('a guest cannot edit an entry', function () {
     $guestbookPost = GuestbookPost::factory()->create();
--- tests/Feature/PublicationDateTest.php
+++ tests/Feature/PublicationDateTest.php
@@ -4,41 +4,9 @@ declare(strict_types=1);
 
 use App\Models\PublicationDate;
 use App\Models\User;
-use Carbon\Carbon;
 use function Pest\Laravel\actingAs;
 use function Pest\Laravel\get;
 use function Pest\Laravel\put;
-
-beforeEach(function () {
-    $dates = [
-        '2021-11-18',
-        '2021-11-25', // not available anymore
-        '2021-12-02', // oldest date in the archive
-        '2021-12-09',
-        '2021-12-16',
-        '2021-12-23',
-        '2021-12-30',
-        '2022-01-06',
-        '2022-01-13',
-        '2022-01-20',
-        '2022-01-27',
-        '2022-02-03',
-        '2022-02-10',
-        '2022-02-17',
-        '2022-02-24',
-        '2022-03-03',
-        '2022-03-10',
-        '2022-03-17', // newest date in the archive
-        '2022-03-24', // current
-        '2022-03-31', // future cartoon
-    ];
-
-    foreach ($dates as $date) {
-        PublicationDate::factory()->create(['publish_on' => $date]);
-    }
-
-    Carbon::setTestNow('2022-03-26 14:30:00');
-});
 
 test('a guest cannot view all published cartoons', function () {
     get('/publication_dates')->assertRedirect('/login');
@@ -54,7 +22,7 @@ test('a user can view all published cartoons', function () {
         ->assertViewHas('dates')
         ->assertSeeText('31. März 2022')
         ->assertDontSeeText('3. Februar 2022')
-        ->assertDontSeeText('18. November 2021');
+        ->assertDontSeeText('25. November 2021');
 });
 
 test('a user can view all published cartoons on page 2', function () {
@@ -63,7 +31,7 @@ test('a user can view all published cartoons on page 2', function () {
         ->assertOk()
         ->assertDontSeeText('31. März 2022')
         ->assertSeeText('3. Februar 2022')
-        ->assertDontSeeText('18. November 2021');
+        ->assertDontSeeText('25. November 2021');
 });
 
 test('a user can view all published cartoons on page 3', function () {
@@ -72,7 +40,7 @@ test('a user can view all published cartoons on page 3', function () {
         ->assertOk()
         ->assertDontSeeText('31. März 2022')
         ->assertDontSeeText('3. Februar 2022')
-        ->assertSeeText('18. November 2021');
+        ->assertSeeText('25. November 2021');
 });
 
 test('a user cannot force a new cartoon for the current publication date', function () {
