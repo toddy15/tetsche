4cdecaef75fa05f9d4b41653316b3981f5d57a41
Dr. Tobias Quathamer <toddy@debian.org>
2023-04-01 20:37:05 +0200

    Remove headline

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -1,8 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1 class="display-1 mb-5">Tetsche-Website</h1>
-
     <div class="text-center">
         <x-image :src="$image_name" class="img-fluid" />
     </div>
