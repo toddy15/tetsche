401aa4f7c0261946cd6ec0d6eb93068c74ff2c5c
Dr. Tobias Quathamer <toddy@debian.org>
2023-06-16 13:41:42 +0200

    Enable plugin

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import manifestSRI from 'vite-plugin-manifest-sri';
 
 export default defineConfig({
-  plugins: [laravel(['resources/js/app.js', 'resources/sass/app.scss'])],
+  plugins: [
+    laravel(['resources/js/app.js', 'resources/sass/app.scss']),
+    manifestSRI()
+  ]
 });
