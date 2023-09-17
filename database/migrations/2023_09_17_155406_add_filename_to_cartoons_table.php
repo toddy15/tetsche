8abe84731a45b810070b4a79ea5841b651d389fd
Dr. Tobias Quathamer <toddy@debian.org>
2023-09-17 15:56:10 +0200

    Add migration for filename

---
+++ database/migrations/2023_09_17_155406_add_filename_to_cartoons_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('cartoons', function (Blueprint $table) {
+            $table->string('filename')->after('id');
+        });
+
+        // Fill the new field with filename data
+        DB::statement(
+            'UPDATE cartoons SET filename=CONCAT(
+                "images/cartoons/",
+                publish_on,
+                ".cartoon.",
+                random_number,
+                ".jpg"
+            )'
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('cartoons', function (Blueprint $table) {
+            $table->dropColumn('filename');
+        });
+    }
+};
