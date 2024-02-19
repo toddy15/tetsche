0c921d7a3bcdc1b0db942b927c66b6d165e941b8
Dr. Tobias Quathamer <toddy@debian.org>
2024-02-19 11:07:05 +0100

    Include search icon via img tag, not as inline data

--- resources/views/guestbook_posts/index.blade.php
+++ resources/views/guestbook_posts/index.blade.php
@@ -14,14 +14,11 @@
                             <label class="visually-hidden form-label" for="search">Suche:</label>
                             <div class="input-group">
                                 <input type="text" class="form-control" id="search" name="q"
-                                    placeholder="Suche …">
+                                    placeholder="Suchtext …">
                                 <span class="input-group-btn">
                                     <button class="btn btn-primary" type="submit">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                            fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-                                            <path
-                                                d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-                                        </svg>
+                                        <img src="{{ Vite::asset('resources/images/suche.svg') }}" role="img"
+                                            aria-hidden="true" />
                                         <span class="visually-hidden">Suchen</span>
                                     </button>
                                 </span>
