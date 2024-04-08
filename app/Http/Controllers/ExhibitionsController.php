d301666551e00aa021a0bb9f36849b63196d168a
Dr. Tobias Quathamer <toddy@debian.org>
2024-04-08 14:23:24 +0200

    Generate Exhibition model and supporting files

---
+++ app/Http/Controllers/ExhibitionsController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ExhibitionRequest;
+use App\Http\Requests\UpdateExhibitionRequest;
+use App\Models\Exhibition;
+
+class ExhibitionsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(ExhibitionRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Exhibition $exhibition)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Exhibition $exhibition)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(ExhibitionRequest $request, Exhibition $exhibition)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Exhibition $exhibition)
+    {
+        //
+    }
+}
