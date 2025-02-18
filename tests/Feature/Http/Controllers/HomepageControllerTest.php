80cb189ed4bce87adf1dedb66bba0de11f60c39a
Dr. Tobias Quathamer <toddy@debian.org>
2025-02-18 22:33:32 +0100

    Remove book from startpage

--- tests/Feature/Http/Controllers/HomepageControllerTest.php
+++ tests/Feature/Http/Controllers/HomepageControllerTest.php
@@ -6,10 +6,9 @@ use function Pest\Laravel\get;
 
 test('the homepage returns ok', function () {
     get(route('homepage'))
-        ->assertOk();
-    // @ TODO reenable after sonderseite has been deactivated
-    // ->assertViewIs('pages.homepage')
-    // ->assertViewHas('src')
-    // ->assertViewHas('width')
-    // ->assertViewHas('height');
+        ->assertOk()
+        ->assertViewIs('pages.homepage')
+        ->assertViewHas('src')
+        ->assertViewHas('width')
+        ->assertViewHas('height');
 });
