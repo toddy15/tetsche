b7a0e76611cfe03fb0f3300958eb458a157acde4
Dr. Tobias Quathamer <toddy@debian.org>
2025-07-04 14:29:35 +0200

    Run pint

--- tests/Feature/Http/Controllers/HomepageControllerTest.php
+++ tests/Feature/Http/Controllers/HomepageControllerTest.php
@@ -5,10 +5,10 @@ declare(strict_types=1);
 test('the homepage returns ok', function () {
     $this->get(route('homepage'))
         ->assertOk();
-        /*
-        ->assertViewIs('pages.homepage')
-        ->assertViewHas('src')
-        ->assertViewHas('width')
-        ->assertViewHas('height');
-        */
+    /*
+    ->assertViewIs('pages.homepage')
+    ->assertViewHas('src')
+    ->assertViewHas('width')
+    ->assertViewHas('height');
+    */
 });
