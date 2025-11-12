176057e3d115b546213d3429d9caf077a5ee5abb
Dr. Tobias Quathamer <toddy@debian.org>
2025-11-12 16:18:07 +0100

    Update homepage

--- tests/Feature/Http/Controllers/HomepageControllerTest.php
+++ tests/Feature/Http/Controllers/HomepageControllerTest.php
@@ -4,11 +4,9 @@ declare(strict_types=1);
 
 test('the homepage returns ok', function () {
     $this->get(route('homepage'))
-        ->assertOk();
-    /*
-    ->assertViewIs('pages.homepage')
-    ->assertViewHas('src')
-    ->assertViewHas('width')
-    ->assertViewHas('height');
-    */
+        ->assertOk()
+        ->assertViewIs('pages.homepage')
+        ->assertViewHas('src')
+        ->assertViewHas('width')
+        ->assertViewHas('height');
 });
