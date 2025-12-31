c15319cd75237c14443e41c4da18d23bcc24006f
Dr. Tobias Quathamer <toddy@debian.org>
2025-12-31 15:57:09 +0100

    Update test to pass again

--- tests/Feature/Http/Controllers/HomepageControllerTest.php
+++ tests/Feature/Http/Controllers/HomepageControllerTest.php
@@ -5,10 +5,7 @@ declare(strict_types=1);
 test('the homepage returns ok', function () {
     $this->get(route('homepage'))
         ->assertOk()
-        ->assertViewIs('pages.sonderseite');
-    /*
         ->assertViewHas('src')
         ->assertViewHas('width')
         ->assertViewHas('height');
-    */
 });
